a66eedf5a048c9a0d636598f5687a22b395a1c1f
asharikov <asharikov@bmstu.ru>
2018-10-04 15:13:37 +0300

    Product's page

--- components/com_licensing/models/product.php
+++ components/com_licensing/models/product.php
@@ -20,17 +20,25 @@ class LicensingModelProduct extends ItemModel
     {
         $arr = array();
         $db =& $this->getDbo();
+        $format = LicensingHelper::getParams("format_date_site", '%d.%m.%Y');
         $query = $db->getQuery(true);
-        $query->select('`s`.`name` as `product`, `l`.`name` as `license`, `l`.`dogovor`, `s`.`countAvalible` as `cnt`, `s`.`unlim`, `l`.`dateExpires`, `s`.`about`')
+        $query->select('`s`.`name` as `product`, `s`.`unlim`, `l`.`unlim` as `unlimLic`, `s`.`about`')
+            ->select('`l`.`name` as `license`, `l`.`number` as `number`, `l`.`dogovor`')
+            ->select('`s`.`countAvalible` as `cnt`, `s`.`count` as `buy`')
+            ->select("DATE_FORMAT(`l`.`dateStart`,'{$format}') as `dateStart`, DATE_FORMAT(`l`.`dateExpires`,'{$format}') as `dateExpires`")
             ->from('#__licensing_software as `s`')
             ->leftJoin('#__licensing_licenses as `l` on `l`.`id` = `s`.`licenseID`')
             ->where("`s`.`id` = {$this->id}");
         $result = $db->setQuery($query, 0, 1)->loadObject();
-        $arr['dateExpires'] = $result->dateExpires;
+        $dateStart = $result->dateStart;
+        $dateExpires = ($result->unlimLic != 1) ? $result->dateExpires : JText::_('COM_LICENSING_LICENSES_LIC_SOFT_UNLIM');
+        $arr['dates'] = sprintf("%s: %s - %s", JText::_('COM_LICENSING_SOFTWARE_DATES'), $dateStart, $dateExpires);
         $arr['description'] = $result->about;
         $arr['product'] = $result->product;
         $arr['license'] = (!empty($result->license)) ? $result->license : JText::_('COM_LICENSING_LICENSES_LIC_NUMBER_NO');
+        $arr['number'] = (!empty($result->number)) ? $result->number : JText::_('COM_LICENSING_LICENSES_LIC_NUMBER_NO');
         $arr['dogovor'] = (!empty($result->dogovor)) ? $result->dogovor : JText::_('COM_LICENSING_LICENSES_LIC_NO_INFO');
+        $arr['buy'] = ($result->unlim != 1) ? $result->buy : JText::_('COM_LICENSING_LICENSES_LIC_SOFT_UNLIM');
         $arr['cnt'] = ($result->unlim != 1) ? $result->cnt : JText::_('COM_LICENSING_LICENSES_LIC_SOFT_UNLIM');
         return $arr;
     }
--- components/com_licensing/views/product/tmpl/default.php
+++ components/com_licensing/views/product/tmpl/default.php
@@ -9,7 +9,22 @@
         <h2><?php echo $this->item['product'];?></h2>
     </div>
     <div>
-        <?php echo $this->item['cnt'];?>
+        <h5><?php echo JText::_('COM_LICENSING_LICENSES_LIC_NAME'), ": ", $this->item['license'];?></h5>
+    </div>
+    <div>
+        <h5><?php echo JText::_('COM_LICENSING_LICENSES_LIC_NUMBER'), ": ", $this->item['number'];?></h5>
+    </div>
+    <div>
+        <h5><?php echo JText::_('COM_LICENSING_LICENSES_LIC_DOGOVOR'), ": ", $this->item['dogovor'];?></h5>
+    </div>
+    <div>
+        <h5><?php echo JText::_('COM_LICENSING_SOFTWARE_BYIED'), ": ", $this->item['buy'];?></h5>
+    </div>
+    <div>
+        <h5><?php echo JText::_('COM_LICENSING_SOFTWARE_AVALIABLE'), ": ", $this->item['cnt'];?></h5>
+    </div>
+    <div>
+        <h5><?php echo $this->item['dates'];?></h5>
     </div>
     <div>
         <?php echo $this->item['description'];?>
