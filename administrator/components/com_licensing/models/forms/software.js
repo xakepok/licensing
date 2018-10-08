8f5a6f8eeeef02f1a7f6278f659346f7aff4f03b
asharikov <asharikov@bmstu.ru>
2018-10-08 15:16:13 +0300

    Autocomplete avaliable values

--- administrator/components/com_licensing/models/forms/software.js
+++ administrator/components/com_licensing/models/forms/software.js
@@ -16,9 +16,23 @@ function setUnlim()
         av.disabled = false;
     }
 }
-
+function setAvaliable()
+{
+    var cnt = document.querySelector("#jform_count");
+    var av = document.querySelector("#jform_countAvalible");
+    av.value = cnt.value;
+    av.select();
+}
 window.onload = function () {
     var unlim = document.getElementById("jform_unlim");
+    var cnt = document.querySelector("#jform_count");
     setUnlim();
     unlim.onchange = setUnlim;
+    var url_string = window.location.href; // www.test.com?filename=test
+    var url = new URL(url_string);
+    var id = url.searchParams.get("id");
+    if (id == undefined)
+    {
+        cnt.onchange = setAvaliable;
+    }
 };
