b4bc4a8db8b1046be5dda0fcf8d639cc88201a2c
asharikov <asharikov@bmstu.ru>
2018-10-10 12:40:06 +0300

    Pretty filter

--- administrator/components/com_licensing/views/keys/tmpl/default_filter.php
+++ administrator/components/com_licensing/views/keys/tmpl/default_filter.php
@@ -1,10 +1,36 @@
-<div>
-    <label for="filter_search"><?php echo JText::_('JSEARCH_FILTER_LABEL'); ?></label>
-    <input type="text" autocomplete="off" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo JText::_('COM_LICENSING_FILTER_SOFTWARE'); ?>" />
-    <?php echo LicensingHtmlFilters::state($this->state->get('filter.state')); ?>
-    <?php echo LicensingHtmlFilters::license($this->state->get('filter.license')); ?>
-    <?php echo LicensingHtmlFilters::keytype($this->state->get('filter.keytype')); ?>
-    <?php echo LicensingHtmlFilters::software($this->state->get('filter.software')); ?>
-    <button type="submit"><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
-    <button type="button" onclick="document.getElementById('filter_search').value='';this.form.submit();"><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
+<div class="clearfix">
+    <div class="js-stools-container-bar">
+        <label for="filter_search" class="element-invisible"><?php echo JText::_('JSEARCH_FILTER_LABEL'); ?></label>
+        <div class="btn-wrapper input-append">
+            <input type="text" autocomplete="off" name="filter_search" id="filter_search"
+                   value="<?php echo $this->escape($this->state->get('filter.search')); ?>"
+                   title aria-label="<?php echo JText::_('COM_LICENSING_FILTER_SOFTWARE'); ?>"
+                   data-original-title="<?php echo JText::_('COM_LICENSING_FILTER_SOFTWARE'); ?>"
+                   placeholder="<?php echo JText::_('COM_LICENSING_FILTER_SOFTWARE'); ?>"
+            >
+            <button type="submit" class="btn hasTooltip">
+                <?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>
+                <span class="icon-search" aria-hidden="true"></span>
+            </button>
+        </div>
+        <div class="js-stools-field-filter">
+            <?php echo LicensingHtmlFilters::state($this->state->get('filter.state')); ?>
+        </div>
+        <div class="js-stools-field-filter">
+            <?php echo LicensingHtmlFilters::license($this->state->get('filter.license')); ?>
+        </div>
+        <div class="js-stools-field-filter">
+            <?php echo LicensingHtmlFilters::keytype($this->state->get('filter.keytype')); ?>
+        </div>
+        <div class="js-stools-field-filter">
+            <?php echo LicensingHtmlFilters::software($this->state->get('filter.software')); ?>
+        </div>
+        <div class="btn-wrapper">
+            <button type="button" class="btn hasTooltip js-stools-btn-clear"
+                    onclick="document.getElementById('filter_search').value='';this.form.submit();">
+                <?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>
+            </button>
+        </div>
+    </div>
+    <div class="js-stools-container-list hidden-phone hidden-tablet shown" style=""></div>
 </div>
